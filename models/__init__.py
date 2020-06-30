3108538bbbefd84ed0d096f11452d661e47edf9b
danielj32 <danieljuanito21@hotmail.com>
2020-06-30 21:15:02 +0000

    add new files

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" init for class FileStorage """
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Class BaseModel """
+from datetime import datetime
+from uuid import uuid4
+import models
+
+
+class BaseModel:
+    """ construct """
+
+    def __init__(self, *args, **kwargs):
+        """ Construct """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self):
+        """ String """
+        return('[' + self.__class__.__name__ + '] (' + self.id +
+               ') ' + str(self.__dict__))
+
+    def save(self):
+        """ save function """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ Return a dictonary """
+        aux_dict = self.__dict__.copy()
+        aux_dict['__class__'] = self.__class__.__name__
+        aux_dict['created_at'] = self.created_at.isoformat()
+        aux_dict['updated_at'] = self.updated_at.isoformat()
+        return aux_dict
