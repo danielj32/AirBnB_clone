c7412dbe51ef15788f7ead4ecab12bbc1eb0f04a
danielj32 <danieljuanito21@hotmail.com>
2020-06-30 21:16:28 +0000

    add new class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" class FileStorage
+    serializes instances to a JSON file
+    and deserializes JSON file to instances """
+import json
+import uuid
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """ construct """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ return dictionary objects """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ sets in dictionary the obj with key <obj class name>.id """
+        k = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[k] = obj
+
+    def save(self):
+        """ serializes objectss to the JSON file (path: __file_path) """
+        dic_object = {}
+        with open(FileStorage.__file_path, 'w') as fname:
+            for key, val in FileStorage.__objects.items():
+                dic_object[key] = val.to_dict()
+            fname.write(json.dumps(dic_object))
+
+    def reload(self):
+        """ Reload the file """
+        try:
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
+                l_json = json.load(fname)
+            for key, val in l_json.items():
+                val = ['__class__'](**val)
+                FileStorage.__objects[key] = val
+        except:
+            pass
