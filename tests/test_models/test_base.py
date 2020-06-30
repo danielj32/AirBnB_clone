cf97b3a785be2bffabe8c6419cd2c61415168bd3
danielj32 <danieljuanito21@hotmail.com>
2020-06-30 23:59:48 +0000

    add new class check style new class

--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -5,11 +5,14 @@ import inspect
 import pep8
 from models.base_model import BaseModel
 
+
 class BaseModel_testing(unittest.TestCase):
 
     def testpep8(self):
         """ testing codestyle """
         pepstylecode = pep8.StyleGuide(quiet=True)
-        rest = pepstylecode.check_files(['models/base_model.py'])
+        rest = pepstylecode.check_files(['models/base_model.py',
+                                         'models/__init__.py',
+                                         'models/engine/file_storage.py'])
         self.assertEqual(rest.total_errors, 0,
-                        "Found code style errors (and warnings).")
+                         "Found code style errors (and warnings).")
