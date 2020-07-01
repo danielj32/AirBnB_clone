3f128213adc56701256d6ceec03c1e90bedcf6b5
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-01 16:01:11 +0000

    [ADD] User class

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" User Class """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class that inherits BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
