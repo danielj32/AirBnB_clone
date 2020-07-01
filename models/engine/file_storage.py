35e1fb62f998daaad9e2762b0ace3ae19bf6c7ac
danielj32 <danieljuanito21@hotmail.com>
2020-07-01 16:58:03 +0000

    add serialization User, Place, State, City, Amenity and Review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,12 @@ import uuid
 import os
 from datetime import datetime
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
