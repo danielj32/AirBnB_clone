4fc9424b19fbac4379cdc13e40c3222bb13f835e
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-02 00:26:30 +0000

    [ADD] Tests for state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" testing State """
+import unittest
+import pep8
+from models.state import State
+
+class State_testing(unittest.TestCase):
+    """ check BaseModel """
+
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/state.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
