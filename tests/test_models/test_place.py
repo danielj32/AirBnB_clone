5db08410c10ba9e3f84dc593452712a07536370f
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-02 00:38:01 +0000

    [FIX] Correct folder directories

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,7 +10,7 @@ class Place_testing(unittest.TestCase):
     def testpep8(self):
         """ testing codestyle """
         pepstylecode = pep8.StyleGuide(quiet=True)
-        path_user = 'models/user.py'
+        path_user = 'models/place.py'
         result = pepstylecode.check_files([path_user])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
