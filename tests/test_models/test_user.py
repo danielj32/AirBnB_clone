df9abdd7a018fb1a985ad898b94621e6d1d97fdf
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-02 00:23:38 +0000

    [ADD] User tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" testing User """
+import unittest
+import pep8
+from models.user import User
+
+class User_testing(unittest.TestCase):
+    """ check BaseModel """
+
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/user.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
